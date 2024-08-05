2a865c30ef742eb1e045013ef8ae302da6c8e66f
zhramohammadi <zahrakiianfar@gmail.com>
2024-08-05 15:49:50 +0330

    For the sales table, a button to view the full table was placed

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,14 +1,14 @@
 
 import Header from './header/navbar';
 import Carousel from './body/carousel/carousel';
-import Sales from './body/salesChart/salesChart'
+import Sales from './body/salesChart/salesChart';
 function Home(){
     return(
         <>
             <Header />
             <Carousel/>
             <Sales/>
-            
+
         </>
     )
 }
