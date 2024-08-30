4f5fe4362791d4f8cca43742b69cec91770b7d42
zahra <zahrakiianfar@gmail.com>
2024-08-30 23:42:11 +0430

    The navbar buttons were moved to a separate file

---
+++ src/components/data/navbarButton.tsx
@@ -0,0 +1,29 @@
+
+const navbarButton = [
+    {
+        id: 'iranianMovies',
+        buttonName:'فیلم ایرانی'
+    },
+    {
+        id: 'comedyTheater',
+        buttonName:'تئاتر کمدی'
+    },
+    {
+        id: 'theater',
+        buttonName:'تئاتر'
+    },
+    {
+        id: 'kidsAndTeenager',
+        buttonName:'کودک و نوجوان'
+    },
+    {
+        id: 'foreignFilm',
+        buttonName:'فیلم خارجی'
+    },
+    {
+        id: 'artAndExperience',
+        buttonName:'هنر و تجربه'
+    },
+]
+
+export default navbarButton;
