cac18c2daa5a35d62dd0366c5c641f629ef6df09
zahra <zahrakiianfar@gmail.com>
2024-08-30 23:09:59 +0430

    All components related to categories were added

--- src/components/home/body/categories/categories.tsx
+++ src/components/home/body/categories/categories.tsx
@@ -1,10 +1,31 @@
 import IranianMoviesBanner from "./IranianMovie/iranianMoviesbaner.tsx";
 import IranianMovies from "./IranianMovie/iranianMovies.tsx";
+import ComedyTheaterBanner from "./ComedyTheater/ComedyTheaterBanner.tsx";
+import ComedyTheater from "./ComedyTheater/comedyTheater.tsx";
+import TheaterBanner from "./Theater/theaterBanner.tsx";
+import TheaterList from "./Theater/theater.tsx"
+import KidsAndTeenagerBanner from "./KidsAndTeenager/kidsAndTeenagerBanner.tsx";
+import KidsAndTeenager from "./KidsAndTeenager/kidsAndTeenager.tsx";
+import ForeignFilmBanner from "./foreignFilm/foreignFilmBanner.tsx";
+import ForeignFilm from "./foreignFilm/foreignFilm.tsx";
+import ArtAndExperienceBanner from "./ArtAndExperience/artAndExperienceBanner.tsx";
+import ArtAndExperience from "./ArtAndExperience/artAndExperience.tsx";
     function Categories(){
         return(
             <>
                 <IranianMoviesBanner/>
                 <IranianMovies/>
+                <ComedyTheaterBanner/>
+                <ComedyTheater/>
+                <TheaterBanner/>
+                <TheaterList/>
+                <KidsAndTeenagerBanner/>
+                <KidsAndTeenager/>
+                <ForeignFilmBanner/>
+                <ForeignFilm/>
+                <ArtAndExperienceBanner/>
+                <ArtAndExperience/>
+
             </>
         )
     }
