ed770130cedff961ae44a27bf45ab799847e7c8a
zahra <zahrakiianfar@gmail.com>
2024-08-30 23:21:12 +0430

    Added home component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <>
        <Routes>
+                <Route path={'/home'} element={<Home/>}/>
                 <Route path={'/'} element={<Home/>}/>
                 <Route path={'/login'} element={<Login/>}/>
                 <Route path={'/register'} element={<Register/>}/>
