65ee36ac19d4cdc58beddb8741d857677d9c9c45
zahra <zahrakiianfar@gmail.com>
2024-08-30 23:07:10 +0430

    The category of children and teenagers is complete

---
+++ src/components/data/categories/KidsAndTeenager/KidsAndTeenager.tsx
@@ -0,0 +1,86 @@
+const kidsAndTeenager = [
+    {
+        id: 1,
+        title: " کتابخورها ",
+        director: " اسماعیل رحیمی سروش ",
+        rating: 4.7,
+        viewer:3,
+        poster: "images/categoriesOfMovies/KidsAndTeenager/ketabkhorha.webp"
+    },
+    {
+        id:2 ,
+        title: " اتفاقات عجیب 16 سالگی ",
+        director: "شازده کوچولو ",
+        rating: 5,
+        viewer:1,
+        poster: "images/categoriesOfMovies/KidsAndTeenager/etefaghate-jadide-16salegi.webp "
+    },
+    {
+        id: 3,
+        title: " شازده کوچولو ",
+        director: " احمد تیموری ",
+        rating: 3.3,
+        viewer:6,
+        poster: "images/categoriesOfMovies/KidsAndTeenager/shazde-kocholo.webp "
+    },
+    {
+        id:4 ,
+        title: " سفر پر ماجرا ",
+        director: " حجت الله سیفی ",
+        rating: 2.3,
+        viewer:23,
+        poster: "images/categoriesOfMovies/KidsAndTeenager/safar-por-majara.webp "
+    },
+    {
+        id: 5,
+        title: "پاندای کونگفوکار  ",
+        director: " مایکل میچل ",
+        rating: 4.6,
+        viewer:41,
+        poster: "images/categoriesOfMovies/KidsAndTeenager/panda.webp "
+    },
+    {
+        id: 6,
+        title: " خانم قدا گول نخوریا ",
+        director: " میثم میثاقی ",
+        rating: 4.5,
+        viewer:8,
+        poster: "images/categoriesOfMovies/KidsAndTeenager/khanom-ghoda-gol-nakhoria.webp "
+    },
+    {
+        id: 7,
+        title: " میمون ها ",
+        director: " مریم اسکندری ",
+        rating: 4.2,
+        viewer:17,
+        poster: "images/categoriesOfMovies/KidsAndTeenager/meymonha.webp "
+    },
+    {
+        id:8 ,
+        title: " چوپان و چوب جادویی ",
+        director: " محمد حسین داداش نیا ",
+        rating:5 ,
+        viewer:5,
+        poster: "images/categoriesOfMovies/KidsAndTeenager/chopan-va-chob-jadoie.webp "
+    },
+    {
+        id:9 ,
+        title: " آرزوی الاغ ",
+        director: " محمد حسین داداش نیا ",
+        rating: 4,
+        viewer:21,
+        poster: "images/categoriesOfMovies/KidsAndTeenager/arezoye-olagh.webp"
+    },
+
+]
+
+function moviesData (id:number) {
+    const moviesList = kidsAndTeenager.find((item)=>item.id ==id);
+    if(moviesList){
+        return moviesList;
+    }else{
+        throw new Error (`carousel whit id ${id} not found`)
+    }
+}
+
+export {kidsAndTeenager,moviesData};
---
+++ src/components/home/body/categories/KidsAndTeenager/kidsAndTeenager.tsx
@@ -0,0 +1,59 @@
+
+import {Col, Row, Button} from "react-bootstrap";
+import {kidsAndTeenager} from "../../../../data/categories/KidsAndTeenager/KidsAndTeenager.tsx";
+import {GoChevronLeft, GoHeartFill } from "react-icons/go";
+import { LuUser } from "react-icons/lu";
+function KidsAndTeenager(){
+    return(
+        <>
+            <div className="d-flex justify-content-between m-2 mt-lg-5">
+                <div className="button-size">
+                    <Button size={'sm'} variant={'none'}>
+                        <GoChevronLeft className="mx-1"/>
+                        مشاهده همه
+                    </Button>
+                </div>
+                <p className="mx-lg-3 section" id={'kidsAndTeenager'}>کودک و نوجوان</p>
+            </div>
+
+            <Row xs={3} md={3} lg={4} xl={5} className="mx-1">
+                {kidsAndTeenager.map((moviesList) => (
+                    <Col key={moviesList.id} className="mb-1">
+                        <div className="image-container">
+                            <img
+                                className="poster-size"
+                                src={moviesList.poster}
+                                alt={moviesList.title}
+                            />
+                            <div className="caption-poster">
+                                <div className='d-lg-block d-md-block d-none'>
+                                    <div className="d-flex justify-content-between">
+                                    <span className="d-inline-block p-2">
+                                        {moviesList.viewer} <LuUser className="m-1"/>
+                                    </span>
+                                        <span className="d-inline-block p-2">
+                                        {moviesList.rating} <GoHeartFill className="m-1 text-danger"/>
+                                    </span>
+                                    </div>
+                                    <p className="text-center">کارگردان: {moviesList.director}</p>
+                                    </div>
+                                </div>
+                        </div>
+                            <div className="text-center mt-2 text-muted title-size">
+                            <p>{moviesList.title}</p>
+                            </div>
+                    </Col>
+                ))}
+
+                <div className="col-12 d-lg-none d-md-none d-block">
+                    <Button className="w-100" variant={'btn btn-outline-secondary'}>
+                        <GoChevronLeft className="m-1"/>
+                        مشاهده همه
+                    </Button>
+                </div>
+            </Row>
+        </>
+    )
+}
+
+export default KidsAndTeenager;
