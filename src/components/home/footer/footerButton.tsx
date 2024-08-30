bb63235e90b0906de737a880c97b5cc1e73b587d
zahra <zahrakiianfar@gmail.com>
2024-08-30 23:40:09 +0430

    Footer buttons were moved to a separate file

---
+++ src/components/home/footer/footerButton.tsx
@@ -0,0 +1,37 @@
+
+const footerButton = [
+    {
+        id:1,
+        buttonName: 'تماس با سینماتیکت'
+    },
+    {
+        id:2,
+        buttonName: 'درباره ما'
+    },
+    {
+        id:3,
+        buttonName: 'قوانین و مقررات'
+    },
+    {
+        id:4,
+        buttonName: 'آرشیو فیلم'
+    },
+    {
+        id:5,
+        buttonName: 'اکران آنلاین'
+    },
+    {
+        id:6,
+        buttonName: 'برنامه های آینده'
+    },
+    {
+        id:7,
+        buttonName: 'سوالات متداول'
+    },
+    {
+        id:8,
+        buttonName: 'جدول فروش'
+    },
+]
+
+export default footerButton;
