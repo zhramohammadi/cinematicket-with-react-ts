b1b02bbfbea8b4221d78b442414a4d24026195d0
zahra <zahrakiianfar@gmail.com>
2024-08-30 23:41:31 +0430

    The Iranian movie component has been updated

---
+++ src/components/data/categories/IranianMovie/IranianMovie.tsx
@@ -0,0 +1,135 @@
+
+const iraninan  = [
+    {
+        id: 1,
+        title: "سال گربه  ",
+        director: " مصطفی تقی زاده  ",
+        rating: 2.9,
+        viewer: 1493,
+        poster: "images/categoriesOfMovies/IranianMovie/sal-gorbe.webp"
+      },
+      {
+        id: 2,
+        title: "یادگار جنوب",
+        director: "پدرام پور امیری",
+        rating: 3.4,
+        viewer:212,
+        poster: "images/categoriesOfMovies/IranianMovie/yadegare-jonob.webp"
+      },
+      {
+        id: 3,
+        title: " روایات ناتمام سیما",
+        director: "علی رضا صمدی",
+        rating: 3.1,
+        viewer:31,
+        poster: "images/categoriesOfMovies/IranianMovie/revayat-natamam-sima.webp"
+      },
+      {
+        id: 4,
+        title: "عطر آلود",
+        director: "هادی مقدم ",
+        rating: 2.8,
+        viewer:99,
+        poster: "images/categoriesOfMovies/IranianMovie/atr-alod.webp"
+      },
+      {
+        id: 5,
+        title: "این جمعیت قابل کنترل ",
+        director: "محمد متین اوجانی  ",
+        rating: 3.9,
+        viewer:34,
+        poster: "images/categoriesOfMovies/IranianMovie/in-jamiat-ghabele-kontrol.webp"
+      },
+      {
+        id: 6,
+        title: "تگزاس 3",
+        director: "سید مسعود اطیابd  ",
+        rating: 3.9,
+        viewer:1965,
+        poster: "images/categoriesOfMovies/IranianMovie/tegzas-3.webp"
+      },
+      {
+        id: 7,
+        title: "خجالت نکش 2",
+        director: "رضا مقصودی ",
+        rating: 3.1,
+        viewer:658,
+        poster: "images/categoriesOfMovies/IranianMovie/khejalat-nakesh-2 (2).webp"
+      },
+      {
+        id: 8,
+        title: "مست عشق",
+        director: " حسن فتحی ",
+        rating: 3.9,
+        viewer:2520,
+        poster: "images/categoriesOfMovies/IranianMovie/mast-eshgh.webp"
+      },
+      {
+        id: 9,
+        title: "آبی روشن ",
+        director: " بابک خواجه پاشا  ",
+        rating: 3.5,
+        viewer:122,
+        poster: "images/categoriesOfMovies/IranianMovie/abi-roshan.webp"
+      },
+      {
+        id: 10,
+        title: "شور عاشقی  ",
+        director: " دلریوش یاری",
+        rating: 3.8,
+        viewer:66,
+        poster: "images/categoriesOfMovies/IranianMovie/shore-asheghi.webp"
+      },
+      {
+        id: 11,
+        title: "کوچه ژاپنی ها ",
+        director: "  امیر حسین ثقفی",
+        rating: 2.8,
+        viewer:31,
+        poster: "images/categoriesOfMovies/IranianMovie/koche-zhaponi-ha.webp"
+      },
+      {
+        id: 12,
+        title: "تمساح خونی ",
+        director: "  جواد عزتی  ",
+        rating: 3.8,
+        viewer:3751,
+        poster: "images/categoriesOfMovies/IranianMovie/temsah-khoni.webp"
+      },
+      {
+        id: 13,
+        title: "پول پارتی  ",
+        director: " سعید سهیلی   ",
+        rating: 4.4,
+        viewer:17,
+        poster: "images/categoriesOfMovies/IranianMovie/pol-parti.webp"
+      },
+      {
+        id: 14,
+        title: "ساعت 6 صبح  ",
+        director: " مهران مدیری   ",
+        rating: 4.4,
+        viewer:14,
+        poster: "images/categoriesOfMovies/IranianMovie/saat-6-sobh.webp"
+      },
+      {
+        id: 15,
+        title: " مصاعب شیرین 2 ",
+        director: "  جواد عزتی  ",
+        rating: 3.3,
+        viewer:3,
+        poster: "images/categoriesOfMovies/IranianMovie/masaeb-shirin-2.webp"
+      },
+
+];
+
+function moviesData (id:number) {
+    const moviesList = iraninan.find((item)=>item.id ==id);
+    if(moviesList){
+        return moviesList;
+    }else{
+        throw new Error (`carousel whit id ${id} not found`)
+    }
+}
+
+export {iraninan,moviesData};
---
+++ src/components/home/body/categories/IranianMovie/iranianMoviesbaner.tsx
@@ -0,0 +1,57 @@
+import './../../style.css';
+import {Col, Row, Button,Container} from "react-bootstrap";
+import basicInfo from "../../../../data/basicInfoMovies/moviesBanner.tsx";
+import {GoArrowLeft, GoHeartFill} from "react-icons/go";
+
+function iranianMoviesbaner(){
+    const movie = basicInfo.find((movie => movie.id === 1))
+    return(
+        <>
+            <Container className="position-relative mt-4 height-size">
+                <Row>
+                    <Col>
+                        <img
+                            className="imgBack d-lg-block d-none"
+                            src={movie.images.imgBack}
+                            alt="background"
+                        />
+                        <img
+                            className="poster-back d-lg-block d-none mx-3"
+                            src={movie.images.posterimg}
+                            alt="poster"
+                        />
+                        <img
+                            className="d-lg-none d-block img-back-sm"
+                            src={movie.images.imgBackSm}
+                            alt="background"
+                        />
+                        <div className="caption-movie mt-lg-5 text-end col-lg-9 col-11 ">
+                            <h6>{movie.title}</h6>
+                            <p className="mt-lg-3 mt-1">{movie.director}</p>
+                            <div className="mt-lg-3">
+                        <span className="DescriptionTime d-inline-block ">
+                            {movie.rating} <GoHeartFill className="m-1 text-danger" />
+                        </span>
+
+                                <span className="DescriptionTime  d-lg-inline-block d-none">{movie.duration}</span>
+                                <span className="DescriptionTime d-lg-inline-block d-none">{movie.genre}</span>
+
+                                </div>
+                            <div className="mt-2 d-lg-block d-none">
+                                <p className="font-size">{movie.description}</p>
+                            </div>
+                            <div className="d-flex justify-content-lg-start justify-content-end col-9 mx-5">
+                                <Button className="p-1" variant="danger">
+                                    <GoArrowLeft className="mx-2" />
+                                    خرید بلیت
+                                </Button>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+export default iranianMoviesbaner;
